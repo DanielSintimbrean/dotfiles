abf13ca2a9234e382b3c44027ff901fa29bd7acf
Daniel Sintimbrean <102896754+DanielSintimbrean@users.noreply.github.com>
2025-11-18 12:52:43 +0100

    feat: :sparkles: update

--- fish/dot-config/fish/conf.d/fnm.fish
+++ fish/dot-config/fish/conf.d/fnm.fish
@@ -1,8 +1,5 @@
 fnm env --use-on-cd | source
 
-set FNM_COREPACK_ENABLED true
-
-### No need to install packages globally
 alias yarn="corepack yarn"
 alias yarnpkg="corepack yarnpkg"
 alias pnpm="corepack pnpm"
